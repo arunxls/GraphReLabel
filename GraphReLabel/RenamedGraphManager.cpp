95b223400a0545b0381764ecab35bb3cff0ceffa
Arun Krishnakumar <arunxls@gmail.com>
2015-10-31 20:29:55 -0500

    Actually added RenamedGraphManager

---
+++ GraphReLabel/RenamedGraphManager.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "RenamedGraphManager.h"
+
+template<typename T1, typename T2>
+RenamedGraphManager<T1, T2>::RenamedGraphManager(uint32 buffer_size)
+{
+}
+
+template <typename T1, typename T2>
+RenamedGraphManager<T1, T2>::RenamedGraphManager()
+{
+}
+
+template <typename T1, typename T2>
+RenamedGraphManager<T1, T2>::~RenamedGraphManager()
+{
+}
---
+++ GraphReLabel/RenamedGraphManager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "include.h"
+
+template <typename T1, typename T2>
+class RenamedGraphManager
+{
+public:
+    RenamedGraphManager(uint32 buffer_size);
+    RenamedGraphManager();
+    ~RenamedGraphManager();
+};
+
